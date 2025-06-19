c7090f38066814712ffa43e81c03e786e16db407
Paul Mikol <paul.mikol@gmail.com>
2025-06-19 00:11:27 -0500

    services

--- meshserver/rundaq.py
+++ meshserver/rundaq.py
@@ -20,8 +20,6 @@ from DAQ.lib.process import DAQProcess
 
 logger = make_logger("rundaq")
 
-nats_procs = []
-
 def setup_logging():
     logging.basicConfig(
         level=logging.INFO,
@@ -39,31 +37,12 @@ def acquire_lock():
         logger.error("[rundaq] Another instance is already running.")
         sys.exit(1)
 
-async def start_nats_servers():
-    logger.info("[NATSManager] Launching local NATS servers (4222 + 5222)...")
-
-    p1 = subprocess.Popen(["nats-server", "-p", "4222"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    p2 = subprocess.Popen(["nats-server", "-p", "5222"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
-    nats_procs.extend([p1, p2])
-    await asyncio.sleep(1)  # Give servers time to start
 
 def shutdown():
-    logger.info("[rundaq] Shutting down NATS servers...")
-    for proc in nats_procs:
-        if proc.poll() is None:
-            try:
-                proc.terminate()
-                proc.wait(timeout=3)
-                logger.info(f"[rundaq] Terminated NATS process PID={proc.pid}")
-            except subprocess.TimeoutExpired:
-                proc.kill()
-                logger.warning(f"[rundaq] Force-killed unresponsive NATS PID={proc.pid}")
+  pass
 
 async def run_daq():
     logger.info(f"[rundaq] Running with PID={os.getpid()} UID={os.getuid()} CWD={os.getcwd()}")
-    await start_nats_servers()
-
     logger.info("[rundaq] Launching DAQProcess...")
     daq = DAQProcess()
     await daq.run()
